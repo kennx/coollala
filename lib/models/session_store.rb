2030ea5daac95a5860fdd3630f2a269d0fb05bfa
kennx <zhangxjian@me.com>
2013-02-26 11:05:57 +0800

    增加了SessionStore实现了简单的缓存

---
+++ lib/models/session_store.rb
@@ -0,0 +1,29 @@
+module Coollala
+  class SessionStore
+    @@sessions ||= {}
+    private_class_method :new
+    def initialize(key, value, expire)
+      return nil if @@sessions.has_key?(key)
+      @@sessions[key] = Marshal.dump({:data => value, :expire => expire})
+    end
+    def self.create(params={})
+      params[:expire] ||= 360000
+      key = params.delete(:username)
+      data = params.delete(:userdata)
+      expire = params.delete(:expire)
+      return nil if @@sessions.has_key?(key)
+      new(key, data, expire)
+      @@sessions[key]
+    end
+    def self.get(key)
+      if @@sessions.has_key?(key)
+        Marshal.load(@@sessions[key])
+      else
+        nil
+      end
+    end
+    def self.stored_data
+      @@sessions
+    end
+  end
+end
