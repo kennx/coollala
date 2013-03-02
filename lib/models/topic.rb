eff2aa1ca6b2a6b72147b763684d5c89a01eccc0
kennx <zhangxjian@me.com>
2013-03-02 15:39:37 +0800

    加入验证

--- lib/models/topic.rb
+++ lib/models/topic.rb
@@ -1,5 +1,7 @@
+# encoding: utf-8
 class Topic < ActiveRecord::Base
   belongs_to  :user
   belongs_to  :group
-
+  validates_presence_of     :title, :message => "话题标题不能为空"
+  validates_presence_of     :body,  :message => "话题内容不能为空"
 end
