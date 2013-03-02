d760d7f864521cfa68350578fd77e45f684bd535
kennx <zhangxjian@me.com>
2013-03-02 14:11:56 +0800

    新加了注册登陆的测试.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,15 +3,33 @@ require './spec/spec_helper'
 
 
 
-describe "login", :type => :request do
-  it "should be sign_in page respond to " do
+describe "session", :type => :request do
+  it "should sign in for successful." do
     visit '/sign_in'
-    page.should have_field("session[password]")
-    page.should have_field("session[username]")
-    page.should have_button("提交")
+    fill_in "session[username]", :with => "kenn"
+    fill_in "session[password]", :with => "123456"
+    click_button "登录"
+    current_path.should == '/'
   end
-  it "should signed in" do
-    page.driver.follow :put, "/sign_in", Session.new(:password => "123456", :username => "kenn")
-    page.driver.status_code.should == 303
+  it "should return status code: 401" do
+    visit '/admin/groups'
+    page.driver.status_code.should == 401
+  end
+  it "should return status code: 200" do
+    visit '/sign_in'
+    fill_in "session[username]", :with => "kenn"
+    fill_in "session[password]", :with => "123456"
+    click_button "登录"
+    page.visit '/admin/users'
+    page.driver.status_code.should == 200
+  end
+  it "should sign up for successful" do
+    visit '/sign_up'
+    fill_in 'user[name]', :with => "kennx2"
+    fill_in 'user[email]', :with => "kennx2@test.com"
+    fill_in 'user[password]', :with => "123456"
+    fill_in 'user[password_confirmation]', :with => "123456"
+    click_button "注册"
+    current_path.should == '/sign_in'
   end
 end
