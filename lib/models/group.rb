e839f1d7209b1b39486323d0adf08017bd009051
kennx <zhangxjian@me.com>
2013-03-06 21:51:33 +0800

    添加创建小组

--- lib/models/group.rb
+++ lib/models/group.rb
@@ -18,9 +18,21 @@ class Group < ActiveRecord::Base
   validates_length_of       :slug, :too_long => "小组代号太长，不能大于25个字符", :too_short => "小组代号太短，不能小于3个字符", :within => 3..25
   validates_length_of       :name, :too_long => "小组名称太长，不能大于10个字符", :too_short => "小组名称太短，不能小于2个字符", :within => 2..10
 
+  BLACKLIST = ["new", "list", "admin", "root", "admin",
+               "administrator", "create", "sign_up",
+               "sign_in", "update", "destroy", "delete",
+               "redirect", "search", "find"].freeze
+
+
   before_save do
     if self.summary.blank?
       self.summary = "组长很懒没有给这个小组写点什么"
+    end
+  end
+
+  validate do
+    BLACKLIST.map do |bl|
+      errors.add(:slug, "代号名称不合法，请更换") if slug.include?(bl)
     end
   end
 
